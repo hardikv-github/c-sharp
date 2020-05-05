5b4860ffc73fbd40a8881c576ba3fa6936ae1f33
HV <56589683+hardikv-github@users.noreply.github.com>
2020-05-05 11:46:58 +0530

    Update TracingService.cs

--- TracingService.cs
+++ TracingService.cs
@@ -3,3 +3,8 @@
 ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 tracingService.Trace("Start");
 
+
+//Tracing in Workflow
+ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+tracingService.Trace("Start");
+
