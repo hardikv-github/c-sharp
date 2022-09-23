f7fcc0ea0282ec78b99940aa2309cd9fe153862e
HV <56589683+hardikv-github@users.noreply.github.com>
2022-09-23 12:41:46 +0530

    Create GetOptionSetLocalizedLabel.cs

---
+++ GetOptionSetLocalizedLabel.cs
@@ -0,0 +1,20 @@
+public List<OptionSetDetails> GetOptionSetValueLabel(string entityName, string fieldName)
+        {
+            var attReq = new RetrieveAttributeRequest();
+            attReq.EntityLogicalName = entityName;
+            attReq.LogicalName = fieldName;
+            attReq.RetrieveAsIfPublished = true;
+
+            var attResponse = (RetrieveAttributeResponse)Service.Execute(attReq);
+            var attributeMetadata = (EnumAttributeMetadata)attResponse.AttributeMetadata;
+
+            List<OptionSetDetails> listOptions = new List<OptionSetDetails>();
+            foreach (var option in attributeMetadata.OptionSet.Options)
+            {
+                OptionSetDetails obj = new OptionSetDetails();
+                obj.Value = option.Value;
+                obj.Text = option.Label.UserLocalizedLabel.Label;
+                listOptions.Add(obj);
+            }
+            return listOptions;
+        }
